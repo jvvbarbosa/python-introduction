cc9673482cff48fc6cfb6eeb13f6be50ec68eaf5
Joao Barbosa <jvvianabarbosa@gmail.com>
2019-02-13 16:13:15 +0100

    Filter out garbage items in numbers.txt

--- README.md
+++ README.md
@@ -19,5 +19,5 @@ python 2 truncates 3/2 to 1 while python 3 does not and gives 1.5
 
 3) Qualitatively different answer to some question
 
-4)Alternative channel for information flow out of functions
+4) Alternative channel for information flow out of functions
 
--- sum_program.py
+++ sum_program.py
@@ -8,10 +8,31 @@ Created on Wed Feb 13 13:27:18 2019
 from operator import add
 from functools import reduce
 
+
+def is_float(token):
+    try:
+        float(token)
+       
+    except ValueError:
+        return False
+    else:
+        return True
+
+def my_float(f):
+    try:
+        return float(f)
+    except ValueError:
+        return 0.0
+
 with open ('numbers.txt', 'r') as file:
     for line in file:
-        #print(sum(map(float, line.split())))
-        print(reduce(add, map(float, line.split()), 0)) #Optional initial value for reduce adds support for empty line
+            #print(sum(map(float, filter(isfloat, line.split()))))
+            tokens = line.split()
+            valid_tokens = filter(is_float, tokens)
+            numbers = map(float, valid_tokens)
+            print(sum(numbers))
+            #print(sum(map(float, filter(my_float, line.split())))) #All in one line
+        #print(reduce(add, map(float, line.split()), 0)) #Optional initial value for reduce handles empty line
 
 
         
