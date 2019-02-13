5c7e4c03ba835fd4e3197d2436212581345a7bad
Joao Barbosa <jvvianabarbosa@gmail.com>
2019-02-13 14:33:46 +0100

    division.py contains an example of how backwards compatibility from python3 to python 2 is broken on divisions

---
+++ division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 13 14:31:02 2019
+
+@author: trn2503
+"""
+#THIS DOES NOT RUN, COPY TO INTERPRETER IN ORDER TO UNDERSTAND RESULTS
+#ALSO RUN IN PYTHON2
+3/2, 3//2, 3.0/2.0, 3.0//2.0
+from __future__ import division
+3/2, 3//2, 3.0/2.0, 3.0//2.0
